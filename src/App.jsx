ff49d6c7b5b3210f7274baafd4cec7fc8e824c09
muhamadnursiramdan <muhamadnursiramdan@gmail.com>
2023-11-07 10:25:03 +0700

    Add: FormAddPost and Posts inside Main components

--- src/App.jsx
+++ src/App.jsx
@@ -12,10 +12,53 @@ function Header() {
   );
 }
 
+function Main() {
+  return (
+    <main>
+      <FormAddPost />
+      <Posts />
+    </main>
+  );
+}
+
+function FormAddPost() {
+  return (
+    <form>
+      <input placeholder="Post title" />
+      <textarea placeholder="Post body" />
+      <button>Add post</button>
+    </form>
+  );
+}
+
+function Posts() {
+  return (
+    <section>
+      <List />
+    </section>
+  );
+}
+
+function List() {
+  return (
+    <ul>
+      <li>
+        <h3>Title</h3>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum corrupti
+          tempore aut vitae sed modi, debitis optio dolorem! Ipsum dolor ea cum
+          placeat aliquam at libero culpa eius nostrum fugit?
+        </p>
+      </li>
+    </ul>
+  );
+}
+
 function App() {
   return (
     <section>
       <Header />
+      <Main />
     </section>
   );
 }
